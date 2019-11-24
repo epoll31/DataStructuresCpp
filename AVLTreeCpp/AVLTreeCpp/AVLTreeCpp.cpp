6fda84d1744b013cba079d2e4f85effddd61d5c5
Ethan Pollack <epollack31@gmail.com>
2019-11-24 10:30:47 -0800

    Traversals Work in AVLTree

--- AVLTreeCpp/AVLTreeCpp/AVLTreeCpp.cpp
+++ AVLTreeCpp/AVLTreeCpp/AVLTreeCpp.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "AVLTree.h"
+#include <vector>
 
 int main()
 {
 	AVLTree<int> tree;
-	for (int i = 0; i < 1000; i++)
+	for (int i = 0; i < 15; i++)
 	{
-		int newValue = rand() % 1000;
+		int newValue = rand() % 15;
 		if (tree.Contains(newValue))
 		{
 			i--;
@@ -16,10 +17,59 @@ int main()
 		tree.Add(newValue);
 
 	}
-	
-	for (int i = 0; i < 1000; i++)
+
+	std::vector<Node<int>*> preorder = tree.DepthFirstTraversalPreOrder();
+	std::cout << "PreOrder: ";
+	for (auto i = preorder.begin(); i < preorder.end(); i++)
 	{
-		int newValue = rand() % 1000;
+		std::cout << (*i)->Value;
+		if (i != preorder.end() - 1)
+		{
+			std::cout << " ";
+		}
+	}
+	std::cout << std::endl;
+
+	std::vector<Node<int>*> inorder = tree.DepthFirstTraversalInOrder();
+	std::cout << "InOrder: ";
+	for (auto i = inorder.begin(); i < inorder.end(); i++)
+	{
+		std::cout << (*i)->Value;
+		if (i != inorder.end() - 1)
+		{
+			std::cout << " ";
+		}
+	}
+	std::cout << std::endl;
+
+	std::vector<Node<int>*> postorder = tree.DepthFirstTraversalPostOrder();
+	std::cout << "PostOrder: ";
+	for (auto i = postorder.begin(); i < postorder.end(); i++)
+	{
+		std::cout << (*i)->Value;
+		if (i != postorder.end() - 1)
+		{
+			std::cout << " ";
+		}
+	}
+	std::cout << std::endl;
+
+	std::vector<Node<int>*> breadthfirst = tree.BreadthFirstTraversal();
+	std::cout << "BreadthFirst: ";
+	for (auto i = breadthfirst.begin(); i < breadthfirst.end(); i++)
+	{
+		std::cout << (*i)->Value;
+		if (i != breadthfirst.end() - 1)
+		{
+			std::cout << " ";
+		}
+	}
+	std::cout << std::endl;
+
+
+	for (int i = 0; i < 15; i++)
+	{
+		int newValue = rand() % 15;
 		if (!tree.Contains(newValue))
 		{
 			i--;
@@ -27,4 +77,5 @@ int main()
 		}
 		tree.Remove(newValue);
 	}
+
 }
